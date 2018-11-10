f5dcb4f606d1c9a0a4d1c489f094e118ecb8a2e2
Irving Armenta <irvingarmentajd@gmail.com>
2018-11-10 23:04:05 +0900

    codacy fix - double quotes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const root = document.getElementById("root");
 
 render(<App />,root);
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   // react developer tools
   require("preact/debug");
 }
