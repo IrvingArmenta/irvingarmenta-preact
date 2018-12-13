bfc0c05d718cfb526be613cfa57a498b47b64181
Irving Armenta <irvingarmentajd@gmail.com>
2018-12-13 21:54:48 +0900

    Webfont-loader implementation and /dist ignore

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import { h, Component, render } from "preact";
 import App from "./src/app";
+import WebfontLoader from 'preact-webfont-loader';
 const root = document.getElementById("root");
 
-render(<App />,root);
+const config = {
+  google: {
+    families: ['M PLUS 1p:400,900'],
+  }
+};
+
+render(
+  <WebfontLoader config={config}>
+    <App />
+  </WebfontLoader>
+  ,root);
 
 if (process.env.NODE_ENV === "development") {
   // react developer tools
