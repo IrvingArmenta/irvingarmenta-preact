5de293dbd5d479ae7ebebaff38a09bac24731c19
Irving Armenta <irvingarmentajd@gmail.com>
2019-06-25 17:32:02 +0900

    New preact-cli initial setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './style';
+import { Component } from 'preact';
+
+export default class App extends Component {
+
+	render() {
+		return (
+			<div>
+				<h1>Hello, Worldsssss!</h1>
+			</div>
+		);
+	}
+}
