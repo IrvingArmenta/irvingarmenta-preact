01ca4323bd7d09ec2edfde68b414343e7e85212c
Irving Armenta <irvingarmentajd@gmail.com>
2019-07-22 14:23:45 +0900

    Ripple buttons / Language and theme functionality

--- src/helpers/tsHelpers.ts
+++ src/helpers/tsHelpers.ts
@@ -12,7 +12,7 @@ export const getByBracket = <S, T extends keyof S>(obj: S, key: T) => {
  * Set a className based on a boolean prop from the component
  * @param {string} className - string from CSS modules object
  * @param {boolean?} prop - name of the boolean prop that needs to be checked
- * @return {string} the className or empty string; 
+ * @return {string} the className 'className' or empty string ''
  */
 export const ifClass = (className: string, prop?: boolean): string => {
    return prop ? className : '';
